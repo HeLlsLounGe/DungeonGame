0543ba8fd65cd644bdd2195c788dadacd364d1bf
HeLlsLounGe <24.Athan.Yonker@ksd.org>
2024-02-01 10:28:35 -0800

    Nearly finished fighting, added lighting to scene

--- My project/Assets/Encounters/EncounterCont.cs
+++ My project/Assets/Encounters/EncounterCont.cs
@@ -5,14 +5,14 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Enemy", fileName ="New Enemy")]
 public class EncounterCont : ScriptableObject
 {
-    [SerializeField] float health;
-    [SerializeField] float rockChance;
-    [SerializeField] float paperChance;
-    [SerializeField] float scissorsChance;
-    [SerializeField] float atkChance;
-    [SerializeField] float gold;
+    [SerializeField] public float health;
+    [SerializeField] public float rockChance;
+    [SerializeField] public float paperChance;
+    [SerializeField] public float scissorsChance;
+    [SerializeField] public float atkChance;
+    [SerializeField] public float gold;
     [SerializeField] public Sprite enemy;
-    [SerializeField] Animator enemyAnim;
+    [SerializeField] public Animator enemyAnim;
 
     public float Health(float index)
     {
